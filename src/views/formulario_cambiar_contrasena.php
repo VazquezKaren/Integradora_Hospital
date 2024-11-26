87ccce0ab9d0b0583edecfaa7542ce8a3fb6c2fc
VazquezKaren <169114492+VazquezKaren@users.noreply.github.com>
2024-11-26 00:01:53 -0600

    empleado contraseña

--- src/views/formulario_cambiar_contrasena.php
+++ src/views/formulario_cambiar_contrasena.php
@@ -8,21 +8,35 @@ if (session_status() === PHP_SESSION_NONE) {
 
 
 <body>
-    <section>
+    <section class="main-content">
         <div class="content-grid">
-            <div class="contentbox">
+            <div class="contentbox patient-info">
                 <h2>Cambiar Contraseña</h2>
                 <form id="form-cambiar-contrasena" action="../controladores/modificar_contrasena_empleado.php" method="POST" onsubmit="confirmarCambio(event)">
+                    <div class="form-row">
                     <label for="nueva_contrasena">Nueva Contraseña:</label>
                     <input type="password" id="nueva_contrasena" name="nueva_contrasena" required>
                     <br>
+
+                    </div>
                     
+                    <div class="form-row">
                     <label for="confirmar_contrasena">Confirmar Contraseña:</label>
                     <input type="password" id="confirmar_contrasena" name="confirmar_contrasena" required>
                     <br>
-                   
-                    <button type="submit">Actualizar Contraseña</button>
-                    <button type="button" onclick="cancelarCambio()">Cancelar</button>
+                    </div>
+
+                    <div class="form-row">
+                        <div class="form-group">
+                            <button type="submit">Actualizar Contraseña</button>
+                            <button type="button" onclick="cancelarCambio()">Cancelar</button> 
+
+                        </div>
+                  
+
+                    </div>
+                    
+                    
                 </form>
             </div>
         </div>
